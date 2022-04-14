77a207ac73b4d72f64f18760a7a2ec740c49a1fd
Philip Damra <pdamra@dealerinspire.com>
2022-04-14 10:34:19 -0700

    chore: boilerplate for AWS CDK app

---
+++ test/quickstart-boilerplate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QuickstartBoilerplate from '../lib/quickstart-boilerplate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/quickstart-boilerplate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QuickstartBoilerplate.QuickstartBoilerplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
